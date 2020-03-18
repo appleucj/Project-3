babd99fb6b83454ffc7df2360bb506fba3e1e067
Bek Burkhan <bekburkhan@Beks-MacBook-Pro.local>
2020-03-18 13:18:25 -0400

    update

--- models/User.js
+++ models/User.js
@@ -2,6 +2,13 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     email: String,
+    firstName: String,
+    lastName: String,
+    phoneNumber: String,
+    address: String,
+    city: String,
+    State: String,
+    zipCode: String,
     password: String,
     createdAt: {
         type: Date, 
--- server.js
+++ server.js
@@ -9,15 +9,10 @@ const PORT = process.env.PORT || 4000;
 //be able to read the bode ofn a post in json
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-<<<<<<< HEAD
+
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/project3', {useNewUrlParser: true});
-=======
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-}
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/merntodo', { useNewUrlParser: true });
->>>>>>> master
+
 
 //api routes
 app.use("/api/users", userRoutes);
